7530bdef17e6a9a474ffa1e9403e1a5abf3fad24
D.Saravanan <dayalsaravanan@gmail.com>
2020-02-29 14:34:45 +0530

    Bash script to display maximum of two numbers

---
+++ Feb_24/maximum.sh
@@ -0,0 +1,10 @@
+# File: maximum.sh
+# Name: D.Saravanan    Date: 24/02/2020
+# Bash script to display maximum of two numbers
+
+if [ $1 -ge $2 ]
+then
+	echo "$1 is maximum"
+else
+	echo "$2 is maximum"
+fi
