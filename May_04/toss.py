ffb11aeb4975548cc009356b4d18a3df9064ff96
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-13 11:36:53 +0530

    script to find the probability of Heads/Tails

--- May_04/toss.py
+++ May_04/toss.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
 
 import numpy as np
-import num2words as n2w
 from random import randint
 from iteration_utilities import duplicates
 from iteration_utilities import unique_everseen
+from num2words import num2words
 
 print("\n____Probability of tossing coins____")
-coin = int(input("\nEnter the number of coins: "))
-option1 = input("Type L for at least, M for at most, E for extact: ")
+no_coin = int(input("\nEnter the number of coins: "))
+option1 = input("Type L for at least, M for at most, E for exact: ")
 option2 = input("Type H for Heads, T for Tails: ")
 
 if option2 == 'H':
     side = 'Heads'
-    print("[Note: Assigned value 1 for Head and 0 for Tail]")
+    print("[Note: Assigned value 1 for Heads and 0 for Tails]")
 else:
     side = 'Tails'
-    print("[Note: Assigned value 0 for Head and 1 for Tail]")
+    print("[Note: Assigned value 0 for Heads and 1 for Tails]")
 
 data = []
 numTrails = 10_00_000
 
 for m in range(numTrails):
-    flips = [randint(0,1) for n in range(coin)]
+    flips = [randint(0,1) for n in range(no_coin)]
 
     if option2 == 'H':
         for n in flips:
@@ -34,14 +34,13 @@ for m in range(numTrails):
             if n == 0: data.append(1)
             else: data.append(0)
 
-data = np.array([data[i:i+coin] for i in range(0, len(data), coin)]).tolist()
+data = np.array([data[i:i + no_coin] for i in range(0, len(data), no_coin)]).tolist()
 sample_space = list(unique_everseen(duplicates(data)))
 length = len(sample_space)
 print("\nThe sample space has {} total number of outcomes: \n{}".format(length, sample_space))
 
 if option1 == 'L':
     lvalue = int(input("\nAt least number of {}: ".format(side)))
-    lvalue = n2w(lvalue)
 
     count = 0
     for _list_ in sample_space:
@@ -50,6 +49,8 @@ if option1 == 'L':
             sum += n
 
         if sum >= lvalue: count += 1
+
+    lvalue = num2words(lvalue)
 
     print("The Probability of getting at least {} {} = {}\n".format(lvalue, side, count/length))
 
@@ -64,10 +65,12 @@ elif option1 == 'M':
 
         if sum <= mvalue: count += 1
 
+    mvalue = num2words(mvalue)
+
     print("The Probability of getting at most {} {} = {}\n".format(mvalue, side, count/length))
 
 else:
-    evalue = int(input("Exact number of {}: ".format(side)))
+    evalue = int(input("\nExact number of {}: ".format(side)))
  
     count = 0
     for _list_ in sample_space:
@@ -76,5 +79,7 @@ else:
             sum += n
 
         if sum == evalue: count += 1
+
+    evalue = num2words(evalue)
 
     print("The Probability of getting extactly {} {} = {}\n".format(evalue, side, count/length))
