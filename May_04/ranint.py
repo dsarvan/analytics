40cb89125c4eb426d5951910c42b8bdaf734e238
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-13 14:07:53 +0530

    script to find the probability of an integer choosen randomly from 1 to 100

---
+++ May_04/ranint.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import numpy as np
+
+print("\n_An integer is choosen at random out of the integers from 1 to 100_\n")
+
+numTrails = 10_00_000
+
+data = []
+for n in range(numTrails):
+    data.append(np.random.randint(1, 101, size=1))
+
+sample_space = np.unique(data)
+total_outcome = len(sample_space)
+minValue = min(sample_space)
+maxValue = max(sample_space)
+
+print("The Sample space has {} total number of outcomes: \n{}".format(total_outcome,  sample_space))
+
+option1 = input("\nType M for multiples, L for lesser than, G for greater than: ")
+if option1 == 'M': choosen = 'multiples of'
+elif option1 == 'L': choosen = 'lesser than'
+else: choosen = 'greater than'
+option2 = int(input("Enter an integer for which to find values that are {}: ".format(choosen)))
+
+values = []
+for num in sample_space:
+    if option1 == 'M':
+        if num%option2 == 0: values.append(num)
+    elif option1 == 'L':
+        if num < option2: values.append(num)
+    else:
+        if num > option2: values.append(num)
+
+total = len(values)
+
+print("\nFrom {} to {}, there are {} integers that are {} {}: \n{}".format(minValue, maxValue, total, choosen, option2, values))
+print("\nThe Probability that an integer is {} {}: {}\n".format(choosen, option2,  total/total_outcome))
