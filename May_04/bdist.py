1d92fc78e4c23c6c0847ac8ba00312a048139c8e
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-16 21:01:49 +0530

    script for binomial distribution

--- May_04/bdist.py
+++ May_04/bdist.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 
 def fact(n):
     fact = 1
@@ -7,30 +6,39 @@ def fact(n):
         fact *= value
     return(fact)
 
+print("\n__________Binomial Distribution__________\n")
+
 n = int(input("Number of trials: "))
-p = float(input("Probability of getting a success in one trail: "))
+p = float(input("Probability of getting a success in one trial: "))
 option1 = input("Type L for at least, M for at most, E for exact: ")
 option2 = input("Type S for success, F for failure: ")
 
-if option2 == 'S': x = int(input("Number of success desired: "))
-else: x = int(input("Number of failure desired: "))
+if option2 == 'S':
+    x = int(input("Number of success desired: "))
+    chosen = 'success'
+    k = x
+
+else: 
+    x = int(input("Number of failure desired: "))
+    chosen = 'failure' 
+    k = n-x
 
 if option1 == 'L':
     sum = 0
-    while(n >= x):
-        sum += fact(n)/(fact(n-x)*fact(x)) * (p)**(x) * (1-p)**(n-x)
-        x += 1
-
-    print("{:.4f}".format(sum))
+    while(n >= k):
+        sum += fact(n)/(fact(n-k)*fact(k)) * (p)**(k) * (1-p)**(n-k)
+        k += 1
+    
+    print("\nThe probability of getting at least {} {}: {:.4f}\n".format(x, chosen, sum))
     
 if option1 == 'M':
     sum = 0
-    while(x >= 0):
-        sum += fact(n)/(fact(n-x)*fact(x)) * (p)**(x) * (1-p)**(n-x)
-        x -= 1
+    while(k >= 0):
+        sum += fact(n)/(fact(n-k)*fact(k)) * (p)**(k) * (1-p)**(n-k)
+        k -= 1
 
-    print("{:.4f}".format(sum))
+    print("\nThe probability of getting at most {} {}: {:.4f}\n".format(x, chosen, sum))
 
 if option1 == 'E':
-    sum = fact(n)/(fact(n-x)*fact(x)) * (p)**(x) * (1-p)**(n-x)
-    print("{:.4f}".format(sum))
+    sum = fact(n)/(fact(n-k)*fact(k)) * (p)**(k) * (1-p)**(n-k)
+    print("\nThe probability of getting exactly {} {}: {:.4f}\n".format(x, chosen, sum))
