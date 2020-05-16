4a2f86b4ac06ec5382f2a5521575866eaefda38c
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-16 21:02:31 +0530

    script for binomial distribution using scipy.stats.binom

---
+++ May_04/bdistribution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from scipy.stats import binom
+
+# compute survival function (SF)
+# P(X >= 3)
+pls = binom.sf(k=3, n=5, p=0.3, loc=1)
+print("The probability of getting at least 3 success: {:.4f}".format(pls))
+
+# compute cumulative density function (CDF)
+# P(X <= 3)
+pms = binom.cdf(k=3, n=5, p=0.3, loc=0)
+print("The probability of getting at most 3 success: {:.4f}".format(pms))
+
+# compute probability mass function (PMF)
+# P(X = 2)
+pef = binom.pmf(k=2, n=5, p=0.3, loc=0)
+print("The probability of getting exactly 3 failure: {:.4f}".format(pef))
