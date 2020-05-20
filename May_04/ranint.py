18c33f69e1a922f3db3bc3a51bc1038091b4f8e2
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-20 20:08:05 +0530

    script to compute probability that an integer is choosen at random

--- May_04/ranint.py
+++ May_04/ranint.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
-print("\n_An integer is choosen at random out of the integers from 1 to 100_\n")
+print("\n_An integer is chosen at random out of the integers from 1 to 100_\n")
 
 numTrails = 10_00_000
 
@@ -17,10 +17,10 @@ maxValue = max(sample_space)
 print("The Sample space has {} total number of outcomes: \n{}".format(total_outcome,  sample_space))
 
 option1 = input("\nType M for multiples, L for lesser than, G for greater than: ")
-if option1 == 'M': choosen = 'multiples of'
-elif option1 == 'L': choosen = 'lesser than'
-else: choosen = 'greater than'
-option2 = int(input("Enter an integer for which to find values that are {}: ".format(choosen)))
+if option1 == 'M': chosen = 'multiples of'
+elif option1 == 'L': chosen = 'lesser than'
+else: chosen = 'greater than'
+option2 = int(input("Enter an integer for which to find values that are {}: ".format(chosen)))
 
 values = []
 for num in sample_space:
@@ -33,5 +33,5 @@ for num in sample_space:
 
 total = len(values)
 
-print("\nFrom {} to {}, there are {} integers that are {} {}: \n{}".format(minValue, maxValue, total, choosen, option2, values))
-print("\nThe Probability that an integer is {} {}: {}\n".format(choosen, option2,  total/total_outcome))
+print("\nFrom {} to {}, there are {} integers that are {} {}: \n{}".format(minValue, maxValue, total, chosen, option2, values))
+print("\nThe Probability that an integer is {} {}: {}\n".format(chosen, option2,  total/total_outcome))
