26185133b116231a70bd5d4a739049756a7f198a
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-24 13:24:44 +0530

    script to calculate area P(-0.90 < Z < 1.58)

---
+++ May_04/integration1g.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+from scipy.integrate import quad
+
+def integrand(x):
+    return 1/np.sqrt(2*np.pi) * np.exp(-x**2/2)
+
+ans, err = quad(integrand, -0.90, 1.58)
+print("The area under the standard normal curve P(-0.90 < Z < 1.58) is {:.5f}".format(ans))
