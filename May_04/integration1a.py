abfd6c3f822851075ae89e51ce00ca8ee7fb7335
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-05-29 19:34:23 +0530

    script to calculate area P(Z > 2.70)

--- May_04/integration1a.py
+++ May_04/integration1a.py
@@ -2,8 +2,7 @@
 import numpy as np
 from scipy.integrate import quad
 
-def integrand(x):
-    return 1/np.sqrt(2*np.pi) * np.exp(-x**2/2)
+def integrand(x): return 1/np.sqrt(2*np.pi) * np.exp(-x**2/2)
 
 ans, err = quad(integrand, 2.70, np.inf)
-print("The area under the standard normal curve P(Z > 2.70) is {:.5f}".format(ans))
+print("Area under the standard normal curve P(Z > 2.70): {:.5f}".format(ans))
