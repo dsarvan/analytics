6649fdeb6c4c5594f9c400f251ae1bfe243061e6
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-06-06 21:57:37 +0530

    linear regression analysis

--- Jun_03/linregression.py
+++ Jun_03/linregression.py
@@ -25,14 +25,27 @@ sum_xy = (df.iloc[:,0] * df.iloc[:,1]).sum()
 theta2 = (sum_xy - n*np.mean(x)*np.mean(y))/(sum(x**2) - n*np.mean(x)**2)
 theta1 = (n*np.mean(x)*np.mean(y) - theta2*n*np.mean(x)**2)/(n*np.mean(x))
 
-epsilon = [(y - theta1 - theta2*x)**2 for x,y in zip(x,y)]
+print("__________Linear Regression__________")
 
-print(epsilon)
-print(sum(epsilon))
+print("\ntheta1 = {}".format(theta1))
+print("theta2 = {}".format(theta2))
+
+E = [(y - theta1 - theta2*x)**2 for x,y in zip(x,y)]
+
+print("\nSummation of epsilon-square: {}".format(sum(E)))
 
 cal_y = [theta2*x + theta1 for x in x]
 
-plt.plot(x, y, 'r--o', label='Observed Value')
-plt.plot(x, cal_y, 'b--o', label='Calculated Value')
-plt.legend()
-plt.show()
+y = np.array(y)
+
+print("\nObeserved y-values: {}".format(y))
+print("Calculated y-values: {}".format(cal_y))
+
+epsilon = [(y - cal_y) for y,cal_y in zip(y,cal_y)]
+print("\nError term: {}".format(epsilon))
+
+ax=plt.figure().add_subplot(111)
+ax.plot(x, y, 'r--o', label='Observed Value')
+ax.plot(x, cal_y, 'b--o', label='Calculated Value')
+ax.set(xlabel='x', ylabel='y', title='Linear Regression')
+plt.legend(); plt.savefig('linreg.png'); plt.show()
