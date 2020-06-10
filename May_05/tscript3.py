1f00549f51686f524c9a334b3ec8d54e02a9e7cf
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-06-10 13:57:34 +0530

    script to compute t-statistic for two sample unpaired data

--- May_05/tscript3.py
+++ May_05/tscript3.py
@@ -20,7 +20,7 @@ def ttest_and_variance(s1_stdv, s2_stdv):
     else:
         return non_similar_variance(n1,s1_mean,n2,s2_mean)
 
-print("__________Two-sample T-test for unpaired data__________\n")
+print("__________Two-sample t-test for unpaired data__________\n")
 
 sample1 = [float(value) for value in input("Sample_1 values: ").split()]
 sample2 = [float(value) for value in input("Sample_2 values: ").split()]
@@ -49,25 +49,25 @@ print("\nSample 2: \n\t Number of Observations = {} \n\t Mean = {:.5f}".format(n
 print("\t Standard Deviation = {:.5f}".format(s2_stdv))
 print("\t Standard Error of the Mean = {:.5f}".format(std_err2))
 
-# calculation of T-statistics and degrees of freedom
-tstatistics, dof, sp = ttest_and_variance(s1_stdv, s2_stdv)
-print("\nT statistics: {:.5f}".format(tstatistics))
+# calculation of t-statistic and degrees of freedom
+tstatistic, dof, sp = ttest_and_variance(s1_stdv, s2_stdv)
+print("\nt-statistic: {:.5f}".format(tstatistic))
 
 # calculation of Critical values
 tcritical_l = t.ppf(q = los/2, df = dof)
 tcritical_u = -tcritical_l
 print("\nCritical values are {:.5f}, {:.5f}".format(tcritical_l, tcritical_u))
 
-# decision making - using T-statistics and Critical values
-if tstatistics < tcritical_l or tstatistics > tcritical_u:
+# decision making: t-statistic and Critical values
+if tstatistic < tcritical_l or tstatistic > tcritical_u:
     print("Reject the Null hypothesis.")
 else: print("Fail to reject the Null hypothesis.")
 
 # calculation of p-value
-pvalue = 2*t.cdf(tstatistics, df = dof)
+pvalue = 2*t.cdf(tstatistic, df = dof)
 print("\np-value: {:.5f}".format(pvalue))
 
-# decision making - using p-value and level of significance
+# decision making: p-value and level of significance
 if pvalue < los: print("Reject the Null hypothesis.")
 else: print("Fail to reject the Null hypothesis.")
 
@@ -79,13 +79,12 @@ print("\nStandard Error: {:.5f}".format(std_error))
 cnf_int = (s1_mean - s2_mean) + std_error * np.array([tcritical_l, tcritical_u])
 print("Confidence Interval: {}".format(cnf_int))
 
-# plot script
 x1=np.linspace(-10,tcritical_l,1000); y1=t.pdf(x1,df=dof)
 x2=np.linspace(tcritical_u,10,1000);  y2=t.pdf(x2,df=dof)
 x3=np.linspace(-4,4,1000); y3=t.pdf(x3,df=dof)
 ax=plt.figure().add_subplot(111); ax.plot(x3,y3,color='brown')
 ax.fill_between(x1,y1,0,alpha=0.5,color='brown',label='Rejection Region')
 ax.fill_between(x2,y2,0,alpha=0.5,color='brown'); ax.set(xlabel='X',ylabel='P(X)')
-ax.axvline(x=tstatistics,ls='--',c='b',label='T statistics = {:.5f}'.format(tstatistics))
-ax.set(xlim=[-4,4],title="T Distribution (degrees of freedom = {:.0f})".format(dof))
+ax.axvline(x=tstatistic,ls='--',c='b',label='t-statistic = {:.5f}'.format(tstatistic))
+ax.set(xlim=[-4,4],title="t-distribution (degrees of freedom = {:.0f})".format(dof))
 plt.legend(); plt.savefig('tscript3.pdf',dpi=72,bbox_inches='tight'); plt.show()
