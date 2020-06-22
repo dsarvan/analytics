d554dd83210d276f468b8d4de8dcd26e449214c4
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-06-22 17:36:26 +0530

    java program to print a string

---
+++ Jun_22/program.java
@@ -0,0 +1,13 @@
+/* File: program.java
+ * Name: D.Saravanan
+ * Date: 22/06/2020
+ * Comt: Java program to print "Hello, Universe!"
+*/
+
+class program
+{
+		public static void main(String args[])
+		{
+				System.out.println("Hello, Universe!");
+		}
+}
