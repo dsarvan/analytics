f1cd5b7579b047146c70884407685f0992da4a5d
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-06-26 22:34:59 +0530

    program to generate password with numbers

--- Jun_25/password.java
+++ Jun_25/password.java
@@ -1,21 +1,53 @@
+/* File: password.java
+ * Name: D.Saravanan
+ * Date: 25/06/2020
+ * Comt: Program to generate password with numbers
+*/
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class password {
 
+	static int values(int num) {
+
+		int count = 0;
+
+		while (num != 0) {
+
+			num /= 10;
+			count++;
+		}
+
+		return count;
+	}
+
 	static int generate(int n) {
 
 		Random rand = new Random();
 
+		int num = 1; int nvalue; int result = 0;
+
 		for (int i=1; i<=n; i++) {
 
 			int x = rand.nextInt(9);
-			return x;
+			num += x * Math.pow(10,i-1);
+
+			nvalue = values(num);
+
+			if (nvalue == n) {
+
+				result = num;
+			}
+
+			else {
+
+				result = (int) (Math.pow(10,n-1)) + num;
+			}
 		}
 
-		return 0;
+		return result;
 	}
-
 
 	public static void main(String args[]) {
 
@@ -30,12 +62,11 @@ public class password {
 
 		System.out.println("\nRandomly generated OTP:");
 
-		for (int i=1; i <= users; i++) {
+		for (int i=1; i<=users; i++) {
 
 			int value = generate(length);
 
 			System.out.println("User "+ i +" : "+ value);
 		}
-
 	}
 }
