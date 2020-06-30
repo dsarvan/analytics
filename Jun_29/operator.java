c764660d8a0bbc436a5fda559cc20a05c58422c6
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-06-30 20:40:39 +0530

    package for mathematical arithematic operations

--- Jun_29/operator.java
+++ Jun_29/operator.java
@@ -2,42 +2,47 @@ package arithmetic;
 
 public class operator {
 
-	public void x(int num1, int num2) {
+	public void operation(int N, String option, int num[]) {
 
-		switch (selection) {
+		switch (option) {
 
-			case +:
-				System.out.println(num1+num2);
+			case "+":
+				int sum = num[0]+num[1];
+				System.out.println("\nSum of "+num[0] +" and "+ num[1] +" is "+ sum);
 				break;
 
-			case -:
-				System.out.println(num1-num2);
+			case "-":
+				int diff = num[0]-num[1];
+				System.out.println("\nDifference of "+num[0] +" and "+ num[1] +" is "+ diff);
 				break;
 
-			case *:
-				System.out.println(num1*num2);
+			case "*":
+				int prod = num[0]*num[1];
+				System.out.println("\nProduct of "+num[0] +" and "+ num[1] +" is "+ prod);
+				break; 
+
+			case "/":
+				float div = (float) num[0]/num[1];
+				System.out.println("\nQuotient of "+num[0]+"/"+num[1] +" is "+ div);
 				break;
 
-			case /:
-				System.out.println(num1/num2);
+			case "%":
+				int mod = num[0]%num[1];
+				System.out.println("\nReminder of "+num[0]+"/"+num[1] +" is "+ mod);
 				break;
 
-			case %:
-				System.out.println(num1%num2);
+			case "++":
+				System.out.println("\nIncrement of "+num[0] +" by 1 is "+ (num[0]+1));
+				System.out.println("Increment of "+num[1] +" by 1 is "+ (num[1]+1));
 				break;
 
-			case ++:
-				System.out.println(num1+1);
-				System.out.println(num2+1);
-				break;
-
-			case --:
-				System.out.println(num1-1);
-				System.out.println(num2-1);
+			case "--":
+				System.out.println("\nDecrement of "+num[0] +" by 1 is "+ (num[0]-1));
+				System.out.println("Decrement of "+num[1] +" by 1 is "+ (num[1]-1));
 				break;
 
 			default:
-				System.out.println("None");
+				System.out.println("\nInvalid operation, try again.");
 		}
 	}
 }
