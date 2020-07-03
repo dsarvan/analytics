912c3198bc3de1339593dd050f2229f30bdec5d4
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-07-03 13:59:59 +0530

    program to print text in abstract class and method

---
+++ Jul_01/abstrct.java
@@ -0,0 +1,34 @@
+abstract class base {
+
+	abstract void method1();
+
+	static void method2() {
+
+		System.out.println("There are three kinds of truths: data, data analyzed by a frequentist, andBayesian Network statistics.");
+	}
+
+	public void method3() {
+
+		System.out.println("If I had used Bayesian Networks. I would have never gone bankrupt at the age 59 by making bad investments in the stock market.");
+	}
+}
+
+class derived extends base {
+
+	public void method1() {
+
+		System.out.println("There are three kinds of lies: lies, damned lies, and statistics.");
+	}
+}
+
+public class abstrct {
+
+	public static void main(String args[]) {
+
+		derived print = new derived();
+
+		print.method1();
+		print.method2();
+		print.method3();
+	}
+}
