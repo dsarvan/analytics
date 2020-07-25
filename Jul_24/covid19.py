131145a2f181f25cbbe057f1efc549f896c7d467
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-07-25 23:21:26 +0530

    script to anlayse and visualize covid-19 cases

--- Jul_24/covid19.py
+++ Jul_24/covid19.py
@@ -8,9 +8,13 @@ plt.style.use('seaborn-dark')
 df = pd.read_csv('/home/saran/Analytics/Jul_11/time_series_covid19_confirmed_global.csv')
 df = df.drop(['Province/State', 'Lat', 'Long'], axis=1)
 df = df.groupby('Country/Region').sum()
-df = df.T
+
+df['Total'] = df.sum(axis=1)
+df = df.sort_values(by=['Total'], ascending=False).head(5)
+df = df.drop('Total', axis=1)
 
 df = df.rename(columns={'US':'United States'})
+df = df.T
 
-for name in df.columns:
-    print(name)
+plt.figure(); df.plot(kind='bar'); 
+plt.savefig('plot.pdf'); plt.show()
