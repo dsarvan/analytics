34b893cd0bac9e441899d307d6d7268e91d9aab2
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-07-26 21:19:09 +0530

    script to analyse and visualize covid-19 cases

--- Jul_24/covid19.py
+++ Jul_24/covid19.py
@@ -8,13 +8,65 @@ plt.style.use('seaborn-dark')
 df = pd.read_csv('/home/saran/Analytics/Jul_11/time_series_covid19_confirmed_global.csv')
 df = df.drop(['Province/State', 'Lat', 'Long'], axis=1)
 df = df.groupby('Country/Region').sum()
-
-df['Total'] = df.sum(axis=1)
-df = df.sort_values(by=['Total'], ascending=False).head(5)
-df = df.drop('Total', axis=1)
-
-df = df.rename(columns={'US':'United States'})
 df = df.T
+df = df.rename(columns={'US':'United States'})
 
-plt.figure(); df.plot(kind='bar'); 
-plt.savefig('plot.pdf'); plt.show()
+#x = df.index.values
+#y = df.columns.values
+
+#num = 0
+
+#for name in df.columns.values:
+#    name = []
+#    num += 1
+#
+#    for row in df.itertuples():
+#        name.append(row[num])
+#        print(name)
+
+for date in df.index.values:
+    plt.bar(date, df['India'])
+
+plt.show()
+
+
+
+
+#for date, value in df.iterrows():
+    #for val in value:
+    #    if val != 0: print(val)
+
+
+
+    #print(index, row[value])
+    #if value <= length:
+    #    value += 1
+    
+
+#x = np.arange(len(x1))
+#width = 0.35
+#
+#fig, ax = plt.subplots()
+#rects = ax.bar(x - width/2, x1, width, label='Date')
+#
+#def autolabel(rects):
+#    for rect in rects:
+#        height = rect.get_height()
+#        ax.annotate('{}'.format(height),
+#                    xy=(rect.get_x() + rect.get_width() / 2, height),
+#                    xytext=(0, 3), 
+#                    textcoords="offset points",
+#                    ha='center', va='bottom')
+#
+#autolabel(rects)
+#
+#fig.tight_layout()
+#
+#plt.show()
+
+#plt.figure()
+#for value in df['India']:
+#    if value != 0:
+#        plt.figure();
+#        df['India'].plot(kind='barh', x=df.index.values, y=df.India) 
+#        plt.savefig('plot.pdf'); plt.show()
