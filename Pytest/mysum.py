7a06fdbb19687732baaf73fc326e670000e7d725
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-11-29 23:00:56 +0530

    mysum module

---
+++ Pytest/mysum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" mysum module with mysum function"""
+
+def mysum(numbers):
+    """ sum of numbers """
+    summ = 0
+    for num in numbers:
+        summ += num
+    return summ
