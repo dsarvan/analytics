f073220be0cf89d5948bf0488e189973a0f4ec04
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-12-07 22:40:47 +0530

    script to get historical stock price values

--- Project/Codes/analysis.py
+++ Project/Codes/analysis.py
@@ -4,8 +4,15 @@
 # Date: 07/12/2020
 # Script to analyse financial data from NSE
 
+from datetime import date
+from nsepy import get_history
 import pandas as pd
 
 # listed companies in NSE
 nse = pd.read_csv('/home/saran/Analytics/Project/Data/MCAP_31032020_0.csv', index_col=0)
 nse.index.name = None
+
+# historical stock price values
+for name in nse['Symbol']:
+    data = get_history(symbol = name, start = date(2010,1,1), end = date(2019,12,31))
+    data.to_csv('data_{}.csv'.format(name))
