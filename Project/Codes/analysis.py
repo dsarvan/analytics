33a76554cf0a91713d40e213ef0aa3fee1d610f3
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-12-10 22:00:30 +0530

    correlation matrix

--- Project/Codes/analysis.py
+++ Project/Codes/analysis.py
@@ -9,23 +9,26 @@ from nsepy import get_history
 import numpy as np
 import pandas as pd
 
-from scipy.stats import skew
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-darkgrid')
 
-# listed companies in NSE
-nse = pd.read_csv('/home/saran/Analytics/Project/Data/MCAP_31032020_0.csv', index_col=0)
-nse.index.name = None
-
-# historical stock price values
-for name in nse['Symbol']:
-    data = get_history(symbol = name, start = date(2010,1,1), end = date(2019,12,31))
-    data.to_csv('data_{}.csv'.format(name))
-
-# close stock price values
-close = pd.DataFrame()
-for name in nse['Symbol']:
-    data = pd.read_csv('Dataset/data_{}.csv'.format(name), usecols=['Date','Close'])
-    close[['Date', name]] = data[['Date', 'Close']]
-    close.to_csv('closeprice.csv', index=False)
+## listed companies in NSE
+#nse = pd.read_csv('/home/saran/Analytics/Project/Data/MCAP_31032020_0.csv', index_col=0)
+#nse.index.name = None
+#
+## historical stock price values
+#for name in nse['Symbol']:
+#    data = get_history(symbol = name, start = date(2010,1,1), end = date(2019,12,31))
+#    data.to_csv('data_{}.csv'.format(name))
+#
+## close stock price values
+#close = pd.DataFrame()
+#for name in nse['Symbol']:
+#    data = pd.read_csv('Dataset/data_{}.csv'.format(name), usecols=['Date','Close'])
+#    close[['Date', name]] = data[['Date', 'Close']]
+#    close.to_csv('closeprice.csv', index=False)
 
 data = pd.read_csv('closeprice.csv', index_col=0)
 data.index.name = None
@@ -43,3 +46,9 @@ for name in data.columns:
 nprice = pd.DataFrame()
 for name in price.columns:
     nprice[name] = [(price[name][t] - np.mean(price[name]))/np.std(price[name], ddof=1) for t in range(len(price))]
+
+# correlation matrix
+C = nprice.corr()
+
+C.plot(kind='density')
+plt.savefig('distribution.pdf')
