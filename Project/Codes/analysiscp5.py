fb9cd17e1fccfd3a5ddd20797f34443358f8a193
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-12-14 19:46:38 +0530

    calculation of random normal matrix with mean = 0 and std = 1

---
+++ Project/Codes/analysiscp5.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# File: analysis.py
+# Name: D.Saravanan
+# Date: 07/12/2020
+# Script to analyse financial data from NSE
+
+from datetime import date
+from nsepy import get_history
+import numpy as np
+import scipy.linalg as la
+import pandas as pd
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-darkgrid')
+
+## listed companies in NSE
+#nse = pd.read_csv('/home/saran/Analytics/Project/Data/MCAP_31032020_0.csv', index_col=0)
+#nse.index.name = None
+#
+## historical stock price values
+#for name in nse['Symbol']:
+#    data = get_history(symbol = name, start = date(2010,1,1), end = date(2019,12,31))
+#    data.to_csv('data_{}.csv'.format(name))
+#
+## close stock price values
+#close = pd.DataFrame()
+#for name in nse['Symbol']:
+#    data = pd.read_csv('Dataset/data_{}.csv'.format(name), usecols=['Date','Close'])
+#    close[['Date', name]] = data[['Date', 'Close']]
+#    close.to_csv('closeprice.csv', index=False)
+
+data = pd.read_csv('closeprice.csv', index_col=0)
+data.index.name = None
+
+# remove columns with number of NaN greater than 1%
+data = data.loc[:, data.isnull().mean() < .003]
+
+# price change
+price = pd.DataFrame()
+dt = 1
+for name in data.columns:
+    price[name] = [np.log(data[name][t+dt]) - np.log(data[name][t]) for t in range(len(data)-dt)]
+
+# normalized return
+nprice = pd.DataFrame()
+for name in price.columns:
+    nprice[name] = [(price[name][t] - np.mean(price[name]))/np.std(price[name], ddof=1) for t in range(len(price))]
+
+print(nprice.shape)
+
+# correlation matrix
+C = nprice.corr()
+print(C)
+
+#C = (1/len(nprice)) * (nprice.T @ nprice)
+#print(C)
+
+#gprime = (nprice)/np.sqrt(len(nprice))
+#C1 = (gprime.T @ gprime)
+#print(C1)
+
+#A = np.random.normal(loc = 0, scale = 1, size = nprice.shape)
+#A = np.random.randn(size = nprice.shape)
+#print(A)
+#print(A.shape)
+
+A = pd.DataFrame(np.random.normal(loc = 0, scale = 1, size = nprice.shape))
+
+#R = (1/len(A)) * (A.T @ A)
+R = A.corr()
+print(R)
